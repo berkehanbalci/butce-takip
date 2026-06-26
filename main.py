e6c7a464c337dcff884455b93947ad8c85555ea3
Berkehan Balcı <github-berkehanbalci@gmail.com>
2026-06-26 20:40:46 +0300

    Girdi doğrulaması ekle

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ harcamalar = []
 while True:   
     tutar_metni = input("Lutfen harcama giriniz (çikmak için q): ")
     if tutar_metni == "q":
-        break
-    aciklama = input(f"Lutfen harcamanizin ne oldugunu giriniz: ")
+        break    
     tutar = float(tutar_metni)
-    yeniharcama = {"aciklama": aciklama, "tutar": tutar}
-    harcamalar.append(yeniharcama)
-    toplam = toplam + tutar
+    if tutar > 0:
+        aciklama = input(f"Lutfen harcamanizin ne oldugunu giriniz: ")
+        yeniharcama = {"aciklama": aciklama, "tutar": tutar}
+        harcamalar.append(yeniharcama)
+        toplam = toplam + tutar
+    else:
+        print("Lütfen geçerli bir tutar giriniz")    
 if len(harcamalar) > 0:
     for eleman in harcamalar:
         print(f"{eleman['aciklama']}: {eleman['tutar']} TL")  
