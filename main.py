7bc7ca12d0eab38e68e1b8ec5ef11916af95fa68
Berkehan Balcı <github-berkehanbalci@gmail.com>
2026-06-26 20:24:45 +0300

    Sınırsız harcama giriş-çıkış mekanizması ekle

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-harcama_metni = input("Kac harcama gireceksin:")
-harcama = int(harcama_metni)
 toplam = 0
 harcamalar = []
 
-if harcama > 0:
-    for i in range(harcama):   
-        tutar_metni = input(f"Lutfen {i+1}. harcama tutarini giriniz:")
-        aciklama = input(f"Lutfen {i+1}. harcamanizin ne oldugunu giriniz:")
-        tutar = float(tutar_metni)
-        yeniharcama = {"aciklama": aciklama, "tutar": tutar}
-        harcamalar.append(yeniharcama)
-        toplam = toplam + tutar
+while True:   
+    tutar_metni = input("Lutfen harcama giriniz (çikmak için q): ")
+    if tutar_metni == "q":
+        break
+    aciklama = input(f"Lutfen harcamanizin ne oldugunu giriniz: ")
+    tutar = float(tutar_metni)
+    yeniharcama = {"aciklama": aciklama, "tutar": tutar}
+    harcamalar.append(yeniharcama)
+    toplam = toplam + tutar
+if len(harcamalar) > 0:
     for eleman in harcamalar:
         print(f"{eleman['aciklama']}: {eleman['tutar']} TL")  
     print(f"Toplam tutar: {toplam}")    
-    ortalama = toplam / harcama
-    print(f"Ortalama tutar: {ortalama}")
+    ortalama = toplam / len(harcamalar)
+    print(f"Ortalama tutar: {ortalama}")   
 else:
-    print("Lutfen gecerli bir harcama giriniz")        
+    print("Hiç harcama girilmedi")         
 
 
 
