cd445e4499cca7d5ad89f76faf0092d5a7890ebc
Berkehan Balcı <github-berkehanbalci@gmail.com>
2026-06-27 16:53:34 +0300

    Kodu fonksiyonlara bölerek yeniden düzenle

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import json
-harcamalar = []
-try:
-    with open("harcamalar.json", "r", encoding="utf-8") as dosya:
-        harcamalar = json.load(dosya)
-except FileNotFoundError:
-    harcamalar = []  
-while True:   
-    tutar_metni = input("Lutfen harcama giriniz (çikmak için q): ")
-    if tutar_metni == "q":
-        break    
-    tutar = float(tutar_metni)
-    if tutar > 0:
-        aciklama = input(f"Lutfen harcamanizin ne oldugunu giriniz: ")
-        yeniharcama = {"aciklama": aciklama, "tutar": tutar}
-        harcamalar.append(yeniharcama)
-    else:
-        print("Lütfen geçerli bir tutar giriniz") 
-toplam = 0
-for eleman in harcamalar:
-    toplam = toplam + eleman["tutar"]        
-if len(harcamalar) > 0:
+def harcamalari_yükle():
+    try:
+        with open("harcamalar.json", "r", encoding="utf-8") as dosya:
+            harcamalar = json.load(dosya)
+    except FileNotFoundError:
+        harcamalar = []
+    return harcamalar
+def harcamalari_kaydet(harcamalar):
     with open("harcamalar.json", "w", encoding="utf-8") as dosya:
         json.dump(harcamalar, dosya,  ensure_ascii=False)
+def harcamalari_topla(harcamalar):
+    while True:   
+        tutar_metni = input("Lutfen harcama giriniz (çikmak için q): ")
+        if tutar_metni == "q":
+            break    
+        tutar = float(tutar_metni)
+        if tutar > 0:
+            aciklama = input(f"Lutfen harcamanizin ne oldugunu giriniz: ")
+            yeniharcama = {"aciklama": aciklama, "tutar": tutar}
+            harcamalar.append(yeniharcama)
+        else:
+            print("Lütfen geçerli bir tutar giriniz") 
+    return harcamalar
+def rapor_göster(harcamalar, toplam):
     for eleman in harcamalar:
         print(f"{eleman['aciklama']}: {eleman['tutar']} TL")  
-    print(f"Toplam tutar: {toplam}")    
+    print(f"Toplam tutar: {toplam}")
     ortalama = toplam / len(harcamalar)
-    print(f"Ortalama tutar: {ortalama}")   
+    print(f"Ortalama tutar: {ortalama}")
+harcamalar = harcamalari_yükle()
+harcamalar = harcamalari_topla(harcamalar)          
+toplam = 0
+for eleman in harcamalar:
+    toplam = toplam + eleman["tutar"]  
+if len(harcamalar) > 0:
+    harcamalari_kaydet(harcamalar)
+    rapor_göster(harcamalar, toplam)
 else:
-    print("Hiç harcama girilmedi")         
-
-
-
+    print("Hiç harcama girilmedi")
